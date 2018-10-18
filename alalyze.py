db2dd73167ce515fda96b68d19fd4619be70a834
xavieryang <xavieryang2011@gmail.com>
2018-10-18 17:51:17 +0800

    wushuang

--- alalyze.py
+++ alalyze.py
@@ -10,7 +10,7 @@ content = ''
 #读数据
 def read_csv():
 	content = ''
-	with open(r'maoyan.csv','r',encoding='utf_8_sig',newline='') as file_test:
+	with open(r'wushuang.csv','r',encoding='utf_8_sig',newline='') as file_test:
 		reader = csv.reader(file_test)
 		i = 0
 		for row in reader:
@@ -60,7 +60,49 @@ def word_cloud(content):
 	counts = [test[i][1] for i in range(0,len(test))]
 	wordcloud = WordCloud(width=1300,height=620)
 	wordcloud.add('影评词云',codes,counts,word_size_range=[20,100])
-	wordcloud.render('word_cloud.html') 
+	wordcloud.render('word_cloud_wushuang.html') 
+
+	#评论城市可视化
+def city_table(cityName):
+		print(cityName)
+		cityList=list(set(cityName))
+		city_dict = {cityList[i]:0 for i in range(len(cityList))}
+		for i in range(len(cityList)):
+			city_dict[cityList[i]] = cityName.count(cityList[i])
+		sort_dict = sorted(city_dict.items(),key=lambda d:d[1],reverse=True)
+		city_name = []
+		city_num = []
+		for i in range(len(sort_dict)):
+			city_name.append(sort_dict[i][0])
+			city_num.append(sort_dict[i][1])
+		
+		import random
+		from pyecharts import Bar
+		bar = Bar('评论者城市分布')
+		bar.add("",city_name,city_num,is_label_show=True,is_datazoom_show=True)
+		bar.render(d+'/picture/city_bar.html')
+
+def make_word_cloud(content):
+	content = content.replace('无双','')
+	bg = plt.imread(d + r'/static/znn.jpg')
+	#生成
+	wc = WordCloud(
+		background_color='white',
+		width = 890,
+		height=600,
+		mask=bg,
+		max_font_size=150,
+		random_state=50,
+		font_path=d+"/static/simkai.ttf").generate_from_text(content)
+	#为图片设置字体
+	myfont = fm.FontProperties(fname=d+'static/simkai.ttf')
+	bg_color = ImageColorGenerator(bg)
+	#开始画图
+	plt.imshow(wc.recolor(color_func=bg_color))
+	#为云图去掉坐标轴
+	plt.axis('off')
+	wc.to_file(d+'/picture/wc_cloud_c.png')
+	pass
 
 time = []
 nickName = []
@@ -68,5 +110,11 @@ gendar = []
 cityName = []
 score = []
 content = ''
+#词云图
 content = read_csv()
-word_cloud(content)
+# word_cloud(content)
+
+#城市分布
+# city_table(cityName)
+#云图
+make_word_cloud(content)
--- spider.py
+++ spider.py
@@ -8,7 +8,7 @@ class Spider_maoyan():
 	headers = {
 	     "User-Agent": UserAgent(verify_ssl=False).random,
 	     "Host": "m.maoyan.com",
-	     "Referer": "http://m.maoyan.com/movie/1217236/comments?_v_=yes"
+	     "Referer": "http://m.maoyan.com/movie/342166/comments?_v_=yes"
 	}
 	def __init__(self, url,time):
 		self.url = url
@@ -19,13 +19,12 @@ class Spider_maoyan():
 		response_comment = requests.get(self.url, headers=self.headers)
 		json_comment = response_comment.text
 		json_comment = json.loads(json_comment)
-		# print(json_comment)
+		print(self.url)
 		return json_comment
 
 	def getData(self,json_comment):
 		json_response = json_comment["cmts"]
 		listinfo = []
-
 		for data in json_response:
 			cityName = data["cityName"]
 			content = data["content"]
@@ -43,13 +42,13 @@ class Spider_maoyan():
 		pass
 
 	def file_do(self,listinfo):
-		file_size = os.path.getsize(r'maoyan.csv')
+		file_size = os.path.getsize(r'wushuang.csv')
 		if file_size == 0:
 			name = ['城市','评论','昵称','性别','评分','观影时间']
 			file_test = pd.DataFrame(columns=name,data=listinfo) 
-			file_test.to_csv(r'maoyan.csv',encoding='utf_8_sig',index=False)
+			file_test.to_csv(r'wushuang.csv',encoding='utf_8_sig',index=False)
 		else:
-			with open(r'maoyan.csv','a+',encoding='utf_8_sig',newline='') as file_test:
+			with open(r'wushuang.csv','a+',encoding='utf_8_sig',newline='') as file_test:
 				writer = csv.writer(file_test)
 				writer.writerows(listinfo)
 		pass
@@ -58,19 +57,17 @@ def spider_maoyan():
 	# 猫眼电影短评接口
 	offset = 0
 	# 电影是2018.9.21上映的
-	startTime = '2018-09-21'
-	day = [22,23,24,25,26,27,28,29,30,1,2,3,4,5,6]
+	startTime = '2018-10-01'
+	day = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
 	j = 0
 	page_num = int(20000/15)
 	for i in range(page_num):
 		print(i)
-		comment_api = 'http://m.maoyan.com/mmdb/comments/movie/1217236.json?_v_=yes&offset={0}&startTime={1}%2021%3A09%3A31'.format(offset,startTime)
+		comment_api = 'http://m.maoyan.com/mmdb/comments/movie/342166.json?_v_=yes&offset={0}&startTime={1}%2011%3A31%3A51'.format(offset,startTime)
 		s0 = Spider_maoyan(comment_api,startTime)
 		json_comment = s0.get_json()
 		if json_comment["total"] == 0: # 当前时间内评论爬取完成
-			if j < 9:  # 九月份(9天)
-				startTime = '2018-09-%d'%day[j]
-			elif j>= 9 and j < 15:  # 十月份(6天)
+			if j < 16:
 				startTime = '2018-10-%d'%day[j]
 			else: # 全部爬完
 				break
